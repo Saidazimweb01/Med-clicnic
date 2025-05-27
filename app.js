d1ddf6fd78791bd33a0d6d8231416d239f57f175
Saidazimweb01 <saidazimxonakmalxonov30@gmail.com>
2025-05-27 11:57:01 +0500

    add new

--- app.js
+++ app.js
@@ -5,17 +5,125 @@ let elNameInp = document.querySelector(".cite-foot-opinion__fio")
 let elBtnSend = document.querySelector(".nurse-bottom__send")
 let elInpTel = document.querySelector(".nurse-bottom__tel")
 let elInpFio = document.querySelector(".nurse-bottom__name")
+let elBtnOpen = document.querySelector(".headerr__burger__btn")
+let elNavbar = document.querySelector(".headerr__navbar")
+let elListRender = document.querySelector(".service__list")
+let elListStaff = document.querySelector(".experts-container")
+let elListBlogers = document.querySelector(".blogers__list")
+let elServicesList = document.querySelector(".services__list")
 // let elInpTel = document.querySelector(".nurse-bottom__tel")
 
 
+elBtnOpen.addEventListener("click", () => {
+    elNavbar.classList.toggle("navbar--open")
+})
 
 
 
 
-elBtnFoot.onclick = () => {
-    alert(`Ismingiz: ${elNameInp.value}, Telefon raqamingiz: ${elTelInp.value}, Xabaringiz: ${elLetterInp.value} Saqlandi va yuvorildi!`)
+
+
+// elBtnFoot.onclick = () => {
+//     alert(`Ismingiz: ${elNameInp.value}, Telefon raqamingiz: ${elTelInp.value}, Xabaringiz: ${elLetterInp.value} Saqlandi va yuvorildi!`)
+// }
+// elBtnSend.onclick = () => {
+//     alert(`Ismingiz: ${elInpFio.value}, Telefon raqamingiz: ${elInpTel.value} Saqlandi va qabulga yozildingiz!`)
+
+// }
+
+
+
+function renderListService(arr, list) {
+    list.innerHTML = ""
+
+    arr.forEach(element => {
+        list.innerHTML += `
+        <li class="service__item">
+                        <img width="348" height="196" src="${element.image}" alt="${element.description}">
+                        <h3 class="service__name">${element.title}</h3>
+                        <p class="service__info">${element.description}</p>
+                    </li>
+                   
+        `
+    });
 }
-elBtnSend.onclick = () => {
-    alert(`Ismingiz: ${elInpFio.value}, Telefon raqamingiz: ${elInpTel.value} Saqlandi va qabulga yozildingiz!`)
 
+renderListService(servis, elListRender)
+
+
+function renderListStaff(arr, list) {
+    list.innerHTML = ""
+
+    arr.forEach((element) => {
+
+        list.innerHTML += `
+        <div class="expert-card">
+        <img src="${element.image}" alt="${element.description}" />
+        <h3>${element.name}</h3>
+        <p>${element.description}</p>
+        </div>
+        
+        `
+    });
+    // console.log(element.name);
 }
+
+renderListStaff(mutaxasis, elListStaff)
+
+function renderListBlog(arr, list) {
+    list.innerHTML = ""
+
+    arr.forEach((element) => {
+
+        list.innerHTML += `
+        <li class="blogers__item">
+                        <img width="348" height="196" src="${element.image}" alt="${element.description}">
+                        <div class="blogers__text__box">
+                            <h3 class="blogers__name">${element.title}</h3>
+                            <p class="blogers__info">
+                              ${element.description}
+                            </p>
+                        </div>
+
+
+                        <div class="blogers__data__box">
+                            <span class="blogers__data">
+                              ${element.date}
+                            </span>
+                        </div>
+                    </li>
+                   
+        
+        `
+    });
+    // console.log(element.name);
+}
+
+renderListBlog(blog, elListBlogers)
+
+
+// console.log(mutaxasis);  
+
+
+// function renderListSe(arr, list) {
+//     list.innerHTML = ""
+
+//     arr.forEach(element => {
+//         list.innerHTML += `
+//          <div class="service-card">
+//           <div class="service-image">
+//             <img src="${element.image}" alt="${element.description}" />
+//           </div>
+//           <div class="service-content">
+//             <h3>${element.title}</h3>
+//             <p>
+//              ${element.description}
+//             </p>
+//           </div>
+//         </div>
+                   
+//         `
+//     });
+// }
+
+// renderListSe(servis, elServicesList)
--- medclinic.js
+++ medclinic.js
@@ -4,77 +4,77 @@ const servis = [
         title: "Ginekologiya",
         description:
             "Expert Medical Clinicda sizga professional ginekologlar ko‘magi, to‘liq tekshiruvlar va individual yondashuv taklif qilinadi. Biz reproduktiv salomatligingizni saqlash va muammolarni hal qilishda yordam beramiz.",
-        image: "./images/xizmat-img11.svg",
+        image: "./images/ser-11.png",
     },
     {
         id: 2,
         title: "Estetik ginekologiya",
         description:
             "Expert Medical Clinic estetik ginekologiya xizmatlarini taklif qiladi. Bu yo‘nalishda tashqi ko‘rinishni estetik jihatdan yaxshilash, ayollarga o‘ziga ishonch bag‘ishlash maqsad qilinadi.",
-        image: "./images/xizmat-img10.svg",
+        image: "./images/ser-10.png",
     },
     {
         id: 3,
         title: "Dermatokosmetologiya",
         description:
             "Expert Medical klinikasida dermatokosmetologik muolajalar: teri muammolarini bartaraf etish, yoshartirish va boshqa kosmetik xizmatlar ko‘rsatiladi.",
-        image: "./images/xizmat-img9.svg",
+        image: "./images/ser-9.png",
     },
     {
         id: 4,
         title: "Dermatologiya",
         description:
             "Expert Medical klinikasida dermatologik muolajalar: teri, soch va tirnoq muammolarini hal qilishda sizga yordam beramiz.",
-        image: "./images/xizmat-img8.svg",
+        image: "./images/ser-8.png",
     },
     {
         id: 5,
         title: "Trikologiya",
         description:
             "Trikologiya xizmatlari: soch to‘kilishini davolash, bosh terisi muammolarini aniqlash va muolaja qilishda bizga murojaat qiling.",
-        image: "./images/xizmat-img7.svg",
+        image: "./images/ser-7.png",
     },
     {
         id: 6,
         title: "Plastik jarrohlik",
         description:
             "Expert Medicalda plastik jarrohlik: burun, yuz, tana shakllantirish, rinoplastika, liposaktsiya va boshqa muolajalar mavjud.",
-        image: "./images/xizmat-img6.svg",
+        image: "./images/ser-1.png",
     },
     {
         id: 7,
         title: "UTT",
         description:
             "Zamonaviy UTT xizmatlari: yuqori sifatli ultratovush tekshiruvlari orqali ichki organlaringiz holatini to‘liq tahlil qilamiz.",
-        image: "./images/xizmat-img5.svg",
+        image: "./images/ser-2.png",
     },
     {
         id: 8,
         title: "Endokrinologiya",
         description:
             "Gormonlar, qalqonsimon bez va metabolizm muammolarini aniqlash va davolash bo‘yicha endokrinologlarimiz xizmatlaringizda.",
-        image: "./images/xizmat-img4.svg",
+        image: "./images/ser-3.png",
     },
     {
         id: 9,
         title: "Kosmetologiya",
         description:
             "Yuzni tozalash, yoshartirish, parvarishlash va boshqa zamonaviy kosmetologik xizmatlar siz uchun mavjud.",
-        image: "./images/xizmat-img3.svg",
+        image: "./images/ser-4.png",
     },
     {
         id: 10,
         title: "Lazerli kosmetologiya",
         description:
             "Lazer yordamida soch olib tashlash, teri muammolarini bartaraf etish va yoshartirish xizmatlari siz uchun xizmatda.",
-        image: "./images/xizmat-img2.svg",
+        image: "./images/ser-5.png",
     },
     {
         id: 11,
         title: "Laboratoriya tahlillar",
         description:
             "Har xil tibbiy ko‘rsatkichlar bo‘yicha laborator tahlillar: qon, siydik va boshqa namunalar asosida aniq tashxis qo‘yiladi.",
-        image: "./images/xizmat-img1.svg",
+        image: "./images/ser-6.png",
     },
 ];
 
@@ -83,55 +83,55 @@ let mutaxasis = [
         id: 1,
         name: "Nargiza Farmonova",
         description: "Alergolog",
-        image: "./images/mutaxasis-img1.svg",
+        image: "./images/mutaxasis-img1.png",
     },
     {
         id: 2,
         name: "Miromdjan Dinara",
         description: "Sakhalarovchi",
-        image: "./images/mutaxasis-img2.svg",
+        image: "./images/mutaxasis-img2.png",
     },
     {
         id: 3,
         name: "Muminov Iqbol",
         description: "Yutaklar uchun",
-        image: "./images/mutaxasis-img3.svg",
+        image: "./images/mutaxasis-img3.png",
     },
     {
         id: 4,
         name: "Oumoz Avaz Farmonov",
         description: "Dizimli",
-        image: "./images/mutaxasis-img4.svg",
+        image: "./images/mutaxasis-img4.png",
     },
     {
         id: 5,
         name: "Sasteva Shirinova",
         description: "Firenolog",
-        image: "./images/mutaxasis-img5.svg",
+        image: "./images/mutaxasis-img5.png",
     },
     {
         id: 6,
         name: "Magnipova Iroda",
         description: "Dizimli uchun",
-        image: "./images/mutaxasis-img6.svg",
+        image: "./images/mutaxasis-img6.png",
     },
     {
         id: 7,
         name: "Mustao Gulmira",
         description: "Vladimirova",
-        image: "./images/mutaxasis-img7.svg",
+        image: "./images/mutaxasis-img7.png",
     },
     {
         id: 8,
         name: "Tajibkhan Ergashev",
         description: "Teni",
-        image: "./images/mutaxasis-img8.svg",
+        image: "./images/mutaxasis-img8.png",
     },
     {
         id: 9,
         name: "Ummona Xolmatova",
         description: "Alergolog",
-        image: "./images/mutaxasis-img9.svg",
+        image: "./images/mutaxasis-img9.png",
     },
 ];
 
@@ -143,7 +143,7 @@ const blog = [
         description:
             "Вас беспокоит выпадение и облысение? Это распространенное явление, с которым сталкиваются многие люди...",
         date: "2022-03-17 01:35:41",
-        image: "./images/yangilik-img1.svg",
+        image: "./images/yangilik-img1.png",
     },
     {
         id: 13,
@@ -151,7 +151,7 @@ const blog = [
         description:
             "С Наврузом! Это распространенное явление, с которым многие люди сталкиваются многие люди",
         date: "2022-03-17 01:57:39",
-        image: "./images/yangilik-img2.svg",
+        image: "./images/yangilik-img2.png",
     },
     {
         id: 14,
@@ -159,7 +159,7 @@ const blog = [
         description:
             "В современную цифровую эпоху распространение информации через медиа ускорилось...",
         date: "2022-03-19 10:00:03",
-        image: "./images/yangilik-img3.svg",
+        image: "./images/yangilik-img3.png",
     },
     {
         id: 15,
@@ -167,7 +167,7 @@ const blog = [
         description:
             "В 2024 году тренды пластической хирургии отражают изменения в общественных представлениях о красоте.",
         date: "2022-03-19 11:27:41",
-        image: "./images/yangilik-img4.svg",
+        image: "./images/yangilik-img14.png",
     },
     {
         id: 16,
@@ -175,6 +175,6 @@ const blog = [
         description:
             "Друзья, как ваши дела? Сегодня мы поговорим о новой процедуре - лазерное блефаропластика и лазерная шлифовка века...",
         date: "2022-03-19 12:47:43",
-        image: "./images/yangilik-img5.svg",
+        image: "./images/yangilik-img5.png",
     },
 ];
