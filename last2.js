603a9f6da195b2ae6804e51b66edfde47a93eeb7
Saidazimweb01 <saidazimxonakmalxonov30@gmail.com>
2025-05-28 17:10:24 +0500

    add

--- last2.js
+++ last2.js
@@ -1,9 +1,29 @@
 let search = new URLSearchParams(window.location.search).get("id")
 
 let elImg2 = document.querySelector(".plastic__imgg")
+let elList = document.querySelector(".experts-container")
 let elTitle = document.querySelector(".plastic__title")
 let elBtnOpen=document.querySelector(".headerr__burger__btn"),elNavbar=document.querySelector(".headerr__navbar")
 elFioInp = document.querySelector(".cite-foot-opinion__fio"), elTelInp = document.querySelector(".cite-foot-opinion__tel"), elLetterInp = document.querySelector(".cite-foot-opinion__letter"), elSendBtn = document.querySelector(".cite-foot-opinoin__send")
+
+function renderListMutaxasis(array, list) {
+    list.innerHTML = ""
+
+    array.forEach((el) => {
+        list.innerHTML += `
+    <a class="experts-card" href="./nurse.html?id=${el.id}">
+   
+                        <img src="${el.image}" alt="${el.description}" />
+                        <h3 class="expert__title">${el.name}</h3>
+                        <p>${el.description}</p>
+                    </div>
+    </a>
+
+`
+    })
+}
+
+renderListMutaxasis(mutaxasis, elList)
 
 let newFindItem2 = blog.find((el) => {
     return el.id == search
