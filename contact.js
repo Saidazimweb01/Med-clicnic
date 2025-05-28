83301ebae45f25b634dad1c6233050f13be833d7
Saidazimweb01 <saidazimxonakmalxonov30@gmail.com>
2025-05-28 16:09:20 +0500

    add

---
+++ contact.js
@@ -0,0 +1,3 @@
+let elBtnOpen=document.querySelector(".headerr__burger__btn"),elNavbar=document.querySelector(".headerr__navbar")
+
+elBtnOpen.addEventListener("click",()=>{elNavbar.classList.toggle("navbar--open")})
